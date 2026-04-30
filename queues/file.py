ff62d7d2da943cd7c098db1ddf5024dca4dc5f94
Lawani-EJ <elyonjohn60@gmail.com>
2026-04-30 08:44:29 +0100

    Modified queue example

--- queues/file.py
+++ queues/file.py
@@ -1,45 +1,15 @@
-class Queue:
-    def __init__(self, length):
-        self.arr = [0] * length
-        self.front = -1
-        self.rear = -1
-        self.size = length
+# Create empty queue
+queue = []
 
-    def enqueue(self, value):
-        if self.rear == self.size - 1:
-            print("Queue is full")
-            return
+# Enqueue (add to rear)
+queue.append("Task 1")
+queue.append("Task 2")
+queue.append("Task 3")
 
-        if self.front == -1:
-            self.front = 0
+print("Queue:", queue)
 
-        self.rear += 1
-        self.arr[self.rear] = value
+# Dequeue (remove from front)
+removed = queue.pop(0)
 
-    def dequeue(self):
-        if self.front == -1 or self.front > self.rear:
-            print("Queue is empty")
-            return
-
-        item = self.arr[self.front]
-        self.front += 1
-        return item
-
-    def display(self):
-        if self.front == -1 or self.front > self.rear:
-            print("Empty queue")
-        else:
-            print(self.arr[self.front:self.rear+1])
-
-
-q = Queue(5)
-
-q.enqueue(10)
-q.enqueue(20)
-q.enqueue(30)
-
-q.display()
-
-print("Removed:", q.dequeue())
-
-q.display()
+print("Removed:", removed)
+print("Queue now:", queue)
