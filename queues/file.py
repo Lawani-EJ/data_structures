8c6cb6b2ef8d6dc3ecca34036b904dae39e54e7d
Lawani-EJ <elyonjohn60@gmail.com>
2026-05-13 21:00:57 +0100

    Modify queue example

--- queues/file.py
+++ queues/file.py
@@ -1,14 +1,11 @@
-# Create empty queue
 queue = []
 
-# Enqueue (add to rear)
 queue.append("Task 1")
 queue.append("Task 2")
 queue.append("Task 3")
 
 print("Queue:", queue)
 
-# Dequeue (remove from front)
 removed = queue.pop(0)
 
 print("Removed:", removed)
